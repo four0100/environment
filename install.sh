fbb9eacf7bd95a5b5623894d36306167fd4734ee
four0100 <suroot@gmail.com>
2013-08-21 08:44:26 -0700

    updating it

--- install.sh
+++ install.sh
@@ -1,13 +1,15 @@
+# move vimrc into place
+mv .vimrc ~
+
 # Color scheme
 mkdir -p ~/.vim/colors && cd ~/.vim/colors
 wget -O wombat256mod.vim http://www.vim.org/scripts/download_script.php?src_id=13400
-
 
 # Setup Pathogen to manage your plugins
 mkdir -p ~/.vim/autoload ~/.vim/bundle
 curl -so ~/.vim/autoload/pathogen.vim https://raw.github.com/tpope/vim-pathogen/HEAD/autoload/pathogen.vim
 # Now you can install any plugin into a .vim/bundle/plugin-name/ folder
-call pathogen#infect()
+# call pathogen#infect()
 
 # Settings for vim-powerline
 cd ~/.vim/bundle
