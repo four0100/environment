d7b07f3230aa4936027d9504cda170f959defc65
four0100 <suroot@gmail.com>
2013-08-21 08:41:53 -0700

    adding install.sh

---
+++ install.sh
@@ -0,0 +1,22 @@
+# Color scheme
+mkdir -p ~/.vim/colors && cd ~/.vim/colors
+wget -O wombat256mod.vim http://www.vim.org/scripts/download_script.php?src_id=13400
+
+
+# Setup Pathogen to manage your plugins
+mkdir -p ~/.vim/autoload ~/.vim/bundle
+curl -so ~/.vim/autoload/pathogen.vim https://raw.github.com/tpope/vim-pathogen/HEAD/autoload/pathogen.vim
+# Now you can install any plugin into a .vim/bundle/plugin-name/ folder
+call pathogen#infect()
+
+# Settings for vim-powerline
+cd ~/.vim/bundle
+git clone git://github.com/Lokaltog/vim-powerline.git
+
+# Settings for ctrlp
+cd ~/.vim/bundle
+git clone https://github.com/kien/ctrlp.vim.git
+
+# Settings for jedi-vim
+cd ~/.vim/bundle
+git clone git://github.com/davidhalter/jedi-vim.git
